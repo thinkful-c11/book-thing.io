506a8bcfb6c35b6fa89596516e1bbfcd0d79b0c1
Jonathan Fitzgibbon <jonnyfitzgib@gmail.com>
2017-08-23 13:44:44 -0400

    Creat runServer/closeServer functions, create config.js

--- index.js
+++ index.js
@@ -2,9 +2,44 @@ const express = require('express');
 
 const app = express();
 
-app.get('/resource', (req, res) => {
-  res.status(200).json();
+app.get('/api/library', (req, res) => {
+  res.status(200).json([{title: 'me', author: 'me', description: 'This is me', 'id': 1}]);
 });
 
+let server;
+let knex;
 
-module.exports = {app};
+const runServer = (database = DATABASE, port = PORT) => {
+  return new Promise((resolve, reject) => {
+    try {
+      knex = require('knex')(database);
+      server = app.listen(port, () => {
+        resolve();
+      });
+    }
+    catch (err) {
+      reject(err);
+    }
+  });
+}
+
+const closeServer = () => {
+  return knex.destroy().then(() => {
+    return new Promise((resolve, reject) => {
+      server.close(err => {
+        if (err) {
+          return reject(err);
+        }
+        resolve();
+      });
+    });
+  });
+}
+
+if (require.main === module) {
+  runServer().catch(err => {
+    console.error(`Can't start server: ${err}`);
+    throw err;
+  });
+}
+module.exports = {app, runServer, closeServer};
--- test/integration_test.js
+++ test/integration_test.js
@@ -5,22 +5,23 @@ const {app} = require('../index');
 chai.use(chaiHttp);
 
 
-describe("Get endpoint", function() {
-  it('should return a status of 200', function() {
+describe("Get endpoint", () => {
+  it('should return a status of 200', () => {
     let res;
     return chai.request(app)
-           .get('/resource')
+           .get('/api/library')
            .then(_res => {
              res = _res;
              res.should.have.status(200);
            })
   })
 
-  it('should return books with correct fields', function() {
+  it('should return books with correct fields', () => {
     let res;
     return chai.request(app)
-      .get('/resource')
+      .get('/api/library')
       .then(_res => {
+        res = _res;
         res.should.have.status(200);
         res.should.be.json;
         res.body.should.be.an('array');
@@ -31,5 +32,11 @@ describe("Get endpoint", function() {
           book.should.include.keys('title', 'author', 'description', 'id');
         });
       })
+  })
+
+  xit('should draw the data from a database', () => {
+      //pull data from the database
+      //get the data from the endpoint
+      //make sure the data matches
   })
 })
