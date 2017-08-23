3c6efe6f7ad484b39834068b91acff5b6f2f2aa0
Jonathan Fitzgibbon <jonnyfitzgib@gmail.com>
2017-08-23 14:20:01 -0400

    Configure config.js with database

--- config.js
+++ config.js
@@ -1,6 +1,19 @@
-exports.DATABASE = [
-  {title: 'me', author: 'me', description: 'This is me', 'id': 1},
-  {title: 'me', author: 'me', description: 'This is me', 'id': 1},
-  {title: 'me', author: 'me', description: 'This is me', 'id': 1},
-  {title: 'me', author: 'me', description: 'This is me', 'id': 1}
-]
+
+require('dotenv').config();
+
+const DATABASE_URL = process.env.DATABASE_URL || global.DATABASE_URL
+
+exports.DATABASE = {
+  client: 'pg',
+  connection: DATABASE_URL
+  pool: { min: 0, max: 3},
+};
+
+// exports.DATABASE = [
+//   {title: 'me', author: 'me', description: 'This is me', 'id': 1},
+//   {title: 'me', author: 'me', description: 'This is me', 'id': 1},
+//   {title: 'me', author: 'me', description: 'This is me', 'id': 1},
+//   {title: 'me', author: 'me', description: 'This is me', 'id': 1}
+// ]
+
+exports.PORT = process.env.PORT || 8080;
