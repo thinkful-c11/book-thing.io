e9464db195d0202f2f6569a56a4144f4ec2edb69
Jonathan Fitzgibbon <jonnyfitzgib@gmail.com>
2017-08-23 14:19:40 -0400

    Refactor

--- test/integration_test.js
+++ test/integration_test.js
@@ -5,47 +5,49 @@ const {app} = require('../index');
 const {DATABASE} = require('../config');
 chai.use(chaiHttp);
 
+describe('Book-thing.io:', () => {
 
-describe("Get endpoint", () => {
-  it('should return a status of 200', () => {
-    let res;
-    return chai.request(app)
-           .get('/api/library')
-           .then(_res => {
-             res = _res;
-             res.should.have.status(200);
-           })
-  })
+  describe("GET endpoint", () => {
+    it('should return a status of 200', () => {
+      let res;
+      return chai.request(app)
+             .get('/api/library')
+             .then(_res => {
+               res = _res;
+               res.should.have.status(200);
+             });
+    });
 
-  it('should return books with correct fields', () => {
-    let res;
-    return chai.request(app)
-      .get('/api/library')
-      .then(_res => {
-        res = _res;
-        res.should.be.json;
-        res.body.should.be.an('array');
-        res.body.should.have.length.of.at.least(1);
-
-        res.body.forEach(book => {
-          book.should.be.an('object');
-          book.should.include.keys('title', 'author', 'description', 'id');
-        });
-      })
-  })
-
-  it('should draw the data from a database', () => {
+    it('should return books with correct fields', () => {
       let res;
       return chai.request(app)
         .get('/api/library')
         .then(_res => {
           res = _res;
-          res.body.forEach((book, index) => {
-            book.id.should.be.equal(DATABASE[index].id);
-            book.author.should.be.equal(DATABASE[index].author);
-            book.description.should.be.equal(DATABASE[index].description);
-            book.title.should.be.equal(DATABASE[index].title);
-          })
-        })
-  })
+          res.should.be.json;
+          res.body.should.be.an('array');
+          res.body.should.have.length.of.at.least(1);
+
+          res.body.forEach(book => {
+            book.should.be.an('object');
+            book.should.include.keys('title', 'author', 'description', 'id');
+          });
+        });
+    });
+
+    it('should draw the data from a database', () => {
+        let res;
+        return chai.request(app)
+          .get('/api/library')
+          .then(_res => {
+            res = _res;
+            res.body.forEach((book, index) => {
+              book.id.should.be.equal(DATABASE[index].id);
+              book.author.should.be.equal(DATABASE[index].author);
+              book.description.should.be.equal(DATABASE[index].description);
+              book.title.should.be.equal(DATABASE[index].title);
+            })
+          });
+    });
+  });
 })
