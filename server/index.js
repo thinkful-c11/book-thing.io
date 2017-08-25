1649658304e6248ba546aa2bea0e8c52cab47903
tannaurus <contacttannergill@gmail.com>
2017-08-25 12:09:18 -0700

    Updated.

--- server/index.js
+++ server/index.js
@@ -11,6 +11,15 @@ app.get(/^(?!\/api(\/|$))/, (req, res) => {
   res.sendFile(index);
 });
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
 app.get("/api/library", (req, res) => {
   knex
     .select("*")
