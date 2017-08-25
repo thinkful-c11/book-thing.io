a5e1c389c5ea3d91b2c913ea3567bc72dd426e4d
Jonathan Fitzgibbon <jonnyfitzgib@gmail.com>
2017-08-25 15:52:26 -0400

    merge

--- client/src/redux/actions/index.js
+++ client/src/redux/actions/index.js
@@ -10,13 +10,7 @@ export const setLibrary = books => ({
 });
 
 export const fetchLibrary = () => dispatch => {
-<<<<<<< HEAD
-  return fetch("/api/library")
-||||||| merged common ancestors
-  return fetch("http://localhost:4000/api/library")
-=======
   return fetch("http://localhost:8080/api/library" ) 
->>>>>>> 768bc70f36b9ba6119994973ba438181c74bb1af
     .then(response => {
       console.log(response);
       if (!response.ok) {
