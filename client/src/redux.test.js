36eedf335bd933378ada4c541ffe1764ec45973c
tannaurus <contacttannergill@gmail.com>
2017-08-25 13:56:33 -0700

    Both tests passing

--- client/src/redux.test.js
+++ client/src/redux.test.js
@@ -12,23 +12,22 @@ const store = mockStore({
   library: []
 });
 
-// test("the testing action should toggle 'testing' in the state", function() {
-//   store.dispatch(actions.testing());
-//   expect(store.getState().testing).toBe(true);
-// });
+test("the testing action should toggle 'testing' in the state", function() {
+  const expectedActions = { type: "TESTING" };
+  store.dispatch(actions.testing());
+  expect(store.getActions()[0]).toEqual(expectedActions);
+});
 
 test("async action should fetch for the library and set it to state", function() {
   fetch.mockResponse(
     JSON.stringify([{ title: "book1", summary: "the description of book1" }])
   );
-  const expectedActions = [
-    {
-      type: "SET_LIBRARY",
-      books: [{ title: "book1", summary: "the description of book1" }]
-    }
-  ];
+  const expectedActions = {
+    type: "SET_LIBRARY",
+    books: [{ title: "book1", summary: "the description of book1" }]
+  };
   expect(store.getState().library).toHaveLength(0);
   return store.dispatch(actions.fetchLibrary()).then(() => {
-    expect(store.getActions()).toEqual(expectedActions);
+    expect(store.getActions()[1]).toEqual(expectedActions);
   });
 });
