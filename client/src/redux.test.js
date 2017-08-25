0bdf8c8768861913eeb59fc086c2558eda352e0f
tannaurus <contacttannergill@gmail.com>
2017-08-25 13:50:00 -0700

    Async action test mocked with jest-fetch-mock. Everything passing.

--- client/src/redux.test.js
+++ client/src/redux.test.js
@@ -1,14 +1,34 @@
-import store from "./redux";
+import configureMockStore from "redux-mock-store";
+import thunk from "redux-thunk";
 import * as actions from "./redux/actions";
-import "whatwg-fetch";
+global.fetch = require("jest-fetch-mock");
 
-test("the testing action should toggle 'testing' in the state", function() {
-  store.dispatch(actions.testing());
-  expect(store.getState().testing).toBe(true);
+const middlewares = [thunk];
+const mockStore = configureMockStore(middlewares);
+
+const store = mockStore({
+  testing: false,
+  success: false,
+  library: []
 });
 
+// test("the testing action should toggle 'testing' in the state", function() {
+//   store.dispatch(actions.testing());
+//   expect(store.getState().testing).toBe(true);
+// });
+
 test("async action should fetch for the library and set it to state", function() {
+  fetch.mockResponse(
+    JSON.stringify([{ title: "book1", summary: "the description of book1" }])
+  );
+  const expectedActions = [
+    {
+      type: "SET_LIBRARY",
+      books: [{ title: "book1", summary: "the description of book1" }]
+    }
+  ];
   expect(store.getState().library).toHaveLength(0);
-  store.dispatch(actions.fetchLibrary());
-  expect(store.getState().success).toBe(true);
+  return store.dispatch(actions.fetchLibrary()).then(() => {
+    expect(store.getActions()).toEqual(expectedActions);
+  });
 });
