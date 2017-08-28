f8b91e4baa8b621359a86cfd0fd0ff9918497a5a
patricewhite <patricewhite2012@gmail.com>
2017-08-27 22:19:34 -0400

    Changed the based app file

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Provider} from 'react-redux';
+import store from "./redux";
 import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import Library from './Library';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+ReactDOM.render(
+<Provider store={store}>
+  <Library />
+</Provider>, document.getElementById('root'));
