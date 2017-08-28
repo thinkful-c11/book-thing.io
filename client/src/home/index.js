3f99a0de2a8460396dc556d65f1bfa8a4eae4f6d
tannaurus <contacttannergill@gmail.com>
2017-08-28 09:20:24 -0700

    Added CSS to the home page.

--- client/src/home/index.js
+++ client/src/home/index.js
@@ -1,20 +1,39 @@
 import React from "react";
-import {Link} from "react-router-dom";
+import "./home.css";
+
+//components
+import { Link } from "react-router-dom";
 
 export default class Home extends React.Component {
   render() {
     return (
       <section>
-        <section>
+        <header>
           <nav>
-              <Link to="/library"><p>Library</p></Link>
-              <Link to="/recommendations"><p>Recommendations</p></Link>
-              <Link to="/sign-in"><p>Sign in</p></Link>
+            <Link style={{ textDecoration: "none" }} to="/library">
+              <p>Library</p>
+            </Link>
+            <Link style={{ textDecoration: "none" }} to="/recommendations">
+              <p>Recommendations</p>
+            </Link>
+            <Link style={{ textDecoration: "none" }} to="/sign-in">
+              <p>Sign in</p>
+            </Link>
           </nav>
-          <h1>Book Thing</h1>
-        </section>
-        <section>
-          <h2>What are we </h2>
+          <section>
+            <img src="/images/header-img.png" />
+            <div>
+              <h1>
+                The Ultimate Book<br />Recommendation Engine
+              </h1>
+              <button>Get Started</button>
+            </div>
+          </section>
+        </header>
+        <section className="home-body">
+          <div>
+            <h2>What we do</h2>
+          </div>
           <section>
             <div>
               <h3>What is Book-Thing.io</h3>
@@ -22,15 +41,21 @@ export default class Home extends React.Component {
             </div>
             <div>
               <h3>Why Use Book-Thing.io</h3>
-              <p>We give you books you want to read not just the ones that sell the best! </p>
+              <p>
+                We give you books you want to read not just the ones that sell
+                the best!
+              </p>
             </div>
             <div>
               <h3>How to Use Book-Thing.io</h3>
-              <p> Simply Log-in, add books, and create recommendations for other users that have read those books</p>
+              <p>
+                Simply Log-in, add books, and create recommendations for other
+                users that have read those books
+              </p>
             </div>
           </section>
         </section>
-    </section>
+      </section>
     );
   }
 }
