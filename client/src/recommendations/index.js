ea93e60f00153c8a143f8b57223d071a0db47f53
patricewhite <patricewhite2012@gmail.com>
2017-08-28 15:10:42 -0400

    can post a new book on rec page

--- client/src/recommendations/index.js
+++ client/src/recommendations/index.js
@@ -7,22 +7,22 @@ class Recommendations extends React.Component {
   handleSubmit(event) {
     event.preventDefault();
     const newBook = {
-      Title: this.title,
-      Author: this.author,
-      Summary: this.summary
+      Title: this.title.value,
+      Author: this.author.value,
+      Summary: this.summary.value
     }
     this.props.dispatch(actions.setBook(newBook));
   }
 
   render() {
     console.log(this.props);
-    const myBooks = this.props.myBooks.map((mybooks, index) => {
-      return (
-        <li key={myBooks.id}>
-          Title: {myBooks.title}
-        </li>
-      )
-    });
+    // const myBooks = this.props.myBooks.map((mybooks, index) => {
+    //   return (
+    //     <li key={myBooks.id}>
+    //       Title: {myBooks.title}
+    //     </li>
+    //   )
+    // });
 
     return (
       <div>
@@ -32,9 +32,9 @@ class Recommendations extends React.Component {
         <section>
           Your Books
           <form>
-            <input type="text" name="Title" placeholder="Title" ref={(title) => this.title = title.value}/>
-            <input type="text" name="Author" placeholder="Author" ref={(author) => this.author = author.value}/>
-            <textarea name="Summary" placeholder="Summary" ref={(summary) => this.summary = summary.value}/>
+            <input type="text" name="Title" placeholder="Title" ref={title => this.title = title}/>
+            <input type="text" name="Author" placeholder="Author" ref={author => this.author = author}/>
+            <textarea name="Summary" placeholder="Summary" ref={summary => this.summary = summary}/>
             <button type="button" value="submit" onClick={(event) => this.handleSubmit(event)}>Submit</button>
           </form>
           <ul>
