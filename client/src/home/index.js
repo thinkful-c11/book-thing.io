ba716c122e5d1c659922c75f5cf29af1a2619c43
patricewhite <patricewhite2012@gmail.com>
2017-08-28 14:50:07 -0400

    updated with merge

--- client/src/home/index.js
+++ client/src/home/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./home.css";
 
 //components
-import { Link } from "react-router-dom";
+import {Link} from "react-router-dom";
 
 export default class Home extends React.Component {
   render() {
@@ -10,38 +10,27 @@ export default class Home extends React.Component {
       <section>
         <header>
           <nav>
-<<<<<<< HEAD
-            <Link to="/library">
+            <Link style={{
+              textDecoration: "none"
+            }} to="/library">
               <p>Library</p>
             </Link>
-            <Link to="/recommendations">
+            <Link style={{
+              textDecoration: "none"
+            }} to="/recommendations">
               <p>Recommendations</p>
             </Link>
-            <Link to="/sign-in">
-              <p>Sign in</p>
-            </Link>
-          </nav>
-          <h1>Book Thing</h1>
-        </section>
-        <section>
-          <h2>What are we
-          </h2>
-=======
-            <Link style={{ textDecoration: "none" }} to="/library">
-              <p>Library</p>
-            </Link>
-            <Link style={{ textDecoration: "none" }} to="/recommendations">
-              <p>Recommendations</p>
-            </Link>
-            <Link style={{ textDecoration: "none" }} to="/sign-in">
+            <Link style={{
+              textDecoration: "none"
+            }} to="/sign-in">
               <p>Sign in</p>
             </Link>
           </nav>
           <section>
-            <img src="/images/header-img.png" />
+            <img src="/images/header-img.png"/>
             <div>
               <h1>
-                The Ultimate Book<br />Recommendation Engine
+                The Ultimate Book<br/>Recommendation Engine
               </h1>
               <button>Get Started</button>
             </div>
@@ -51,7 +40,6 @@ export default class Home extends React.Component {
           <div>
             <h2>What we do</h2>
           </div>
->>>>>>> frontend
           <section>
             <div>
               <h3>What is Book-Thing.io</h3>
@@ -59,25 +47,15 @@ export default class Home extends React.Component {
             </div>
             <div>
               <h3>Why Use Book-Thing.io</h3>
-<<<<<<< HEAD
-              <p>We give you books you want to read not just the ones that sell the best!
-=======
               <p>
-                We give you books you want to read not just the ones that sell
-                the best!
->>>>>>> frontend
+                We give you books you want to read not just the ones that sell the best!
               </p>
             </div>
             <div>
               <h3>How to Use Book-Thing.io</h3>
               <p>
-<<<<<<< HEAD
-                Simply Log-in, add books, and create recommendations for other users that have read those books</p>
-=======
-                Simply Log-in, add books, and create recommendations for other
-                users that have read those books
+                Simply Log-in, add books, and create recommendations for other users that have read those books
               </p>
->>>>>>> frontend
             </div>
           </section>
         </section>
