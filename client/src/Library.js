ee49f679c9ca1cb78a3be8bf9851b589ebe26eba
patricewhite <patricewhite2012@gmail.com>
2017-08-27 22:20:33 -0400

    Created a view to see books in the database

---
+++ client/src/Library.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import * as actions from './redux/actions';
+import {connect} from 'react-redux' ;
+
+class Library extends React.Component {
+
+  componentDidMount() {
+    this.props.dispatch(actions.fetchLibrary());
+  }
+
+  render(){
+    const books = this.props.books.map((book, index) => {
+      return(
+        <li key={book.id}>
+          Title: {book.title} <br/>
+          Author: {book.author} <br/>
+          Summary: {book.summary}
+        </li>
+      )
+    });
+    console.log(this.props);
+    return(
+      <div>
+        <h1>Library</h1>
+        <ul>
+          {books}
+        </ul>
+      </div>
+    )
+  }
+}
+
+export const mapStateToProps = (state) => ({books: state.library});
+export  default connect(mapStateToProps)(Library);
