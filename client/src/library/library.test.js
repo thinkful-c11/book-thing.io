bf59a01481d00cee59e2cb1f086fa8e8c3cec183
patricewhite <patricewhite2012@gmail.com>
2017-08-29 12:18:04 -0400

    add display books later

---
+++ client/src/library/library.test.js
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import {Provider} from "react-redux";
+import store from "../redux";
+import Library from './index.js';
+
+it("renders without crashing", () => {
+  const div = document.createElement("div");
+  ReactDOM.render(
+    <Provider store={store}>
+    <Library/>
+  </Provider>, div);
+});
+
+//displays books from databse
--- client/src/redux.test.js
+++ client/src/redux.test.js
@@ -6,25 +6,31 @@ global.fetch = require("jest-fetch-mock");
 const middlewares = [thunk];
 const mockStore = configureMockStore(middlewares);
 
-const store = mockStore({
-  testing: false,
-  success: false,
-  library: []
-});
+const store = mockStore({testing: false, success: false, library: []});
 
 test("the testing action should toggle 'testing' in the state", function() {
-  const expectedActions = { type: "TESTING" };
+  const expectedActions = {
+    type: "TESTING"
+  };
   store.dispatch(actions.testing());
   expect(store.getActions()[0]).toEqual(expectedActions);
 });
 
 test("async action should fetch for the library and set it to state", function() {
-  fetch.mockResponse(
-    JSON.stringify([{ title: "book1", summary: "the description of book1" }])
-  );
+  fetch.mockResponse(JSON.stringify([
+    {
+      title: "book1",
+      summary: "the description of book1"
+    }
+  ]));
   const expectedActions = {
     type: "SET_LIBRARY",
-    books: [{ title: "book1", summary: "the description of book1" }]
+    books: [
+      {
+        title: "book1",
+        summary: "the description of book1"
+      }
+    ]
   };
   expect(store.getState().library).toHaveLength(0);
   return store.dispatch(actions.fetchLibrary()).then(() => {
