8e70cb5dc848d913f33a93df172cfe9b0e03d4ad
patricewhite <patricewhite2012@gmail.com>
2017-08-29 16:20:41 -0400

    Working on createbook dispatch test

--- client/src/recommendations/recommendations.test.js
+++ client/src/recommendations/recommendations.test.js
@@ -2,7 +2,19 @@ import React from "react";
 import ReactDOM from "react-dom";
 import {Provider} from "react-redux";
 import store from "../redux";
+import {shallow} from "enzyme";
+import * as actions from "../redux/actions"
 import Recommendations from './index.js';
+
+//mock async createBook action
+const mockCreateBookAction = (book) => {
+  type : 'CREATE_BOOK',
+  book
+};
+
+jest.mock('../redux/actions', () => Object.assign({}, require.requireActual('../redux/actions'), {createBook: jest.fn().mockImplementation(() => {
+    return mockCreateBookAction;
+  })}));
 
 it("renders without crashing", () => {
   const div = document.createElement("div");
@@ -12,4 +24,18 @@ it("renders without crashing", () => {
   </Provider>, div);
 });
 
-it("creates the new book you input", () => {});
+it("dispatches createBook from handleSubmit", () => {
+  const book = {
+    title: "title",
+    author: "author",
+    summary: "summary"
+  };
+
+  const dispatch = jest.fn();
+  shallow(
+    <Provider store={store}>
+      <Recommendations book={book} dispatch={dispatch}/>
+    </Provider>
+  );
+  expect(dispatch).toHaveBeenCalledWith(mockCreateBookAction(book));
+});
