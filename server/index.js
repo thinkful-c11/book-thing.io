7b0d56a21b4fc09798551d78effba9044d348ec1
tannaurus <contacttannergill@gmail.com>
2017-08-31 12:37:49 -0700

    premerge.

--- server/index.js
+++ server/index.js
@@ -100,6 +100,7 @@ app.get("/api/auth/google/callback",
     session: false
   }),
   (req, res) => {
+    console.log(req.user);
     res.cookie('accessToken', req.user.accessToken, {expires: 0});
     res.redirect('/');
   });
