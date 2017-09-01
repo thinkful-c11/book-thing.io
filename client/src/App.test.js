c93f4c406e50a62f73dbd46bed7090f9e65648e7
patricewhite <patricewhite2012@gmail.com>
2017-09-01 12:29:35 -0400

    Added store and provider

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {StaticRouter} from 'react-router-dom';
+import {Provider} from "react-redux";
+import store from "./redux";
 import App from './App';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(
+    <Provider store={store}>
     <StaticRouter>
-    <App/>
-  </StaticRouter>, div);
+      <App/>
+    </StaticRouter>
+  </Provider>, div);
 });
