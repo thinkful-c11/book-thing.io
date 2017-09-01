f1a7b1d2639bb8402bafc7d21f28fa56b5e9d9b3
patricewhite <patricewhite2012@gmail.com>
2017-09-01 12:29:56 -0400

    Added provider and store

--- client/src/home/home.test.js
+++ client/src/home/home.test.js
@@ -1,14 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { MemoryRouter } from "react-router-dom";
+import {MemoryRouter} from "react-router-dom";
+import {Provider} from "react-redux";
+import store from "../redux";
 import Home from "./index.js";
 
 it("renders without crashing", () => {
   const div = document.createElement("div");
   ReactDOM.render(
+    <Provider store={store}>
     <MemoryRouter>
-      <Home />
-    </MemoryRouter>,
-    div
-  );
+      <Home/>
+    </MemoryRouter>
+  </Provider>, div);
 });
