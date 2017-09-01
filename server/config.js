a560964119157b9d45ad77366fb8828a1696ec7a
Jonathan Fitzgibbon <jonnyfitzgib@gmail.com>
2017-09-01 15:43:48 -0400

    Move client id and secret to .env and pull into config file

--- server/config.js
+++ server/config.js
@@ -1,8 +1,10 @@
 require("dotenv").config();
 
 const DATABASE_URL = process.env.DATABASE_URL || global.DATABASE_URL;
-const TEST_DATABASE_URL =
-  process.env.TEST_DATABASE_URL || global.TEST_DATABASE_URL;
+const TEST_DATABASE_URL = process.env.TEST_DATABASE_URL || global.TEST_DATABASE_URL;
+
+exports.CLIENT_ID = process.env.CLIENT_ID || global.CLIENT_ID;
+exports.CLIENT_SECRET = process.env.CLIENT_SECRET || global.CLIENT_SECRET;
 
 exports.TEST_DATABASE = {
   client: "pg",
