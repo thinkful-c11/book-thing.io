beaa987d769ac5c227d1b8d16d31518b53308db5
patricewhite <patricewhite2012@gmail.com>
2017-10-26 15:12:20 -0400

    updated for logging tool

--- client/src/redux/index.js
+++ client/src/redux/index.js
@@ -1,5 +1,15 @@
 import {createStore, applyMiddleware} from "redux";
 import thunk from "redux-thunk";
 import {reducer} from "./reducer";
+const createNodeLogger = require('redux-node-logger');
+// import * as createNodeLogger from "redux-node-logger";
 
-export default createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(), applyMiddleware(thunk));
+export default createStore(reducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(), applyMiddleware(thunk), applyMiddleware(createNodeLogger({
+  downArrow: '▼',
+  rightArrow: '▶',
+  messageColor: 'bright-yellow',
+  prevColor: 'grey',
+  actionColor: 'bright-blue',
+  nextColor: 'green',
+  predicate: ''
+})));
